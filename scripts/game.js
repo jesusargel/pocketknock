e2a86f796451f3038d2ccd9b85abfe0e5afb21b1
Mason Perrella <mperrell@stetson.edu>
2017-02-18 22:43:32 -0500

    reshaping frame

--- scripts/game.js
+++ scripts/game.js
@@ -16,12 +16,12 @@ var render = Render.create({
 });
 
 // create two boxes and a ground
-var ball1 = Bodies.circle(400, 200, 50, 50);
-var ball2 = Bodies.circle(450, 50, 50, 50);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-var wallr = Bodies.rectangle(810,300,60, 600, { isStatic: true});
-var walll = Bodies.rectangle(0,400,60, 810, { isStatic: true});
-var ceil = Bodies.rectangle(400,0,810, 60, { isStatic: true});
+var ball1 = Bodies.circle(400, 200, 25, 25);
+var ball2 = Bodies.circle(450, 50, 25, 25);
+var ground = Bodies.rectangle(400, 610, 690, 60, { isStatic: true });
+var wallr = Bodies.rectangle(810,300,60, 500, { isStatic: true});
+var walll = Bodies.rectangle(0,400,60, 690, { isStatic: true});
+var ceil = Bodies.rectangle(400,0,690, 60, { isStatic: true});
 
 //move ball1
 var mouseconstr = Matter.MouseConstraint.create(engine, Bodies);
