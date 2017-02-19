aa4a07483ac8473aa5217d3bf7c3cefec6c439a1
Jesus Argel <jargel@stetson.edu>
2017-02-19 02:08:10 -0500

    Added new balls

--- scripts/game.js
+++ scripts/game.js
@@ -32,7 +32,11 @@ function moving()
 
 // create two boxes and a ground
 var ball1 = Bodies.circle(350, 300, 25, 25);
-var ball2 = Bodies.circle(450, 300, 25, 25);
+var ball2 = Bodies.circle(450, 320, 25, 25);
+var ball3 = Bodies.circle(550, 310, 25, 25);
+var ball4 = Bodies.circle(250, 370, 25, 25);
+var ball5 = Bodies.circle(430, 490, 25, 25);
+var ball6 = Bodies.circle(470, 130, 25, 25);
 var ground = Bodies.rectangle(415, 610, 625, 60, { isStatic: true });
 ground.restitution = 0.8;
 var wallr = Bodies.rectangle(810,310,60, 425, { isStatic: true});
@@ -69,15 +73,19 @@ ceil.restitution = 0.8;
 document.onkeydown = function(e) {
   r = r || event
   if (r.keyCode == 'r') { // escape
-   	Matter.Bodies.setPostition(ball1, [350, 450]);
-   	Matter.Bodies.setPostition(ball2, [450, 450]);
+   	Matter.Bodies.setPostition(ball1, [350, 300]);
+   	Matter.Bodies.setPostition(ball2, [450, 320]);
+    Matter.Bodies.setPostition(ball3, [550, 310]);
+    Matter.Bodies.setPostition(ball4, [250, 370]);
+    Matter.Bodies.setPostition(ball5, [430, 490]);
+    Matter.Bodies.setPostition(ball6, [470, 130]);
 }
 }
 
 
 engine.world.gravity.y = 0;
 // add all of the bodies to the world
-World.add(engine.world, [ball1, ball2, ground, wallr, walll, ceil,mouseConstraint]);
+World.add(engine.world, [ball1, ball2, ball3, ball4, ball5, ball6, ground, wallr, walll, ceil,mouseConstraint]);
 
 
 // run the engine
