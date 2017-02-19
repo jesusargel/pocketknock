6c1aebe2e19e497a74bd28051e42442c14b785b0
Mason Perrella <mperrell@stetson.edu>
2017-02-18 22:07:42 -0500

    normal js

--- scripts/game.js
+++ scripts/game.js
@@ -16,23 +16,22 @@ var render = Render.create({
 });
 
 // create two boxes and a ground
-var ball1 = Bodies.circle(400, 200, 80, 80);
-var ball2 = Bodies.circle(450, 50, 80, 80);
+var ball1 = Bodies.rectangle(400, 200, 80, 80);
+var ball2 = Bodies.rectangle(450, 50, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-//set world's position
-Matter.Composite.translate(World, 500, [recursive=true])
+//move ball1
+var mouseconstr = Matter.MouseConstraint.create(engine, Bodies);
 
+//if(mouseconstr.ball1)
+//{
+	//Matter.Body.applyForce(ball1, 500, 20);
+//}
 
 // add all of the bodies to the world
 World.add(engine.world, [ball1, ball2, ground]);
 
-var mouseconstr = Matter.MouseConstraint.create(engine, Bodies);
 
-if(mouseconstr.ball1)
-{
-	Matter.Body.applyForce(ball1, 500, 20);
-}
 // run the engine
 Engine.run(engine);
 
