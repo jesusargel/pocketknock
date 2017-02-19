e441191587c07efa52560e79a606213c6bba082f
Mason Perrella <mperrell@stetson.edu>
2017-02-18 21:51:40 -0500

    added balls

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,10 @@
-// module aliases
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
     Bodies = Matter.Bodies;
+    
+
+    
 
 // create an engine
 var engine = Engine.create();
@@ -14,13 +16,23 @@ var render = Render.create({
 });
 
 // create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ball1 = Bodies.circle(400, 200, 80, 80);
+var ball2 = Bodies.circle(450, 50, 80, 80);
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-// add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+//set world's position
+Matter.Composite.translate(World, 500, [recursive=true])
 
+
+// add all of the bodies to the world
+World.add(engine.world, [ball1, ball2, ground]);
+
+var mouseconstr = Matter.MouseConstraint.create(engine, Bodies);
+
+if(mouseconstr.ball1)
+{
+	Matter.Body.applyForce(ball1, 500, 20);
+}
 // run the engine
 Engine.run(engine);
 
